805c9e8007610067cdfa7de42bab20bce1a0b6cd
DavidingPlus <davidingplus@qq.com>
2025-03-10 03:15:57 +0000

    add defs

---
+++ src/defs.h
@@ -0,0 +1,17 @@
+/**
+ * @file defs.h
+ * @author DavidingPlus (davidingplus@qq.com)
+ * @brief 定义全局宏和结构体的头文件。
+ *
+ * Copyright (c) 2024 电子科技大学 刘治学
+ *
+ */
+
+
+#ifndef _NVMIX_DEFS_H_
+#define _NVMIX_DEFS_H_
+
+#define NVMIX_BLOCK_SIZE 4096 // 4 KIB
+
+
+#endif

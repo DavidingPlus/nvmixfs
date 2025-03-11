1b3beec59cb9f33395f103de2756f54497bc691f
DavidingPlus <davidingplus@qq.com>
2025-03-11 08:28:10 +0000

    done nvmixReaddir

--- src/defs.h
+++ src/defs.h
@@ -14,14 +14,23 @@
 #include <linux/fs.h>
 
 
-#define NVMIX_BLOCK_SIZE 4096 // 4 KIB
+#define NVMIX_BLOCK_SIZE 4096    // 数据块的单位，4 KIB。
+#define NVMIX_MAX_ENTRY_NUM 32   // 目录项最多包含的数量。
+#define NVMIX_MAX_NAME_LENGTH 16 // 文件名的最大长度。
 
 
 struct NvmixInodeInfo
 {
     struct inode m_vfsInode;
 
-    __u16 m_dataBlock;
+    unsigned long m_dataBlock;
+};
+
+struct NvmixDirEntry
+{
+    char m_name[NVMIX_MAX_NAME_LENGTH];
+
+    unsigned long m_ino;
 };
 
 
--- src/dir.c
+++ src/dir.c
@@ -23,7 +23,8 @@ struct file_operations nvmixDirOps = {
     // iterate_shared 是共享式遍历，仅持有目录的 inode 共享锁，允许其他进程并发遍历同一目录。
     // 优先使用 iterate_shared，未实现则退回 iterate。
     .read = generic_read_dir,
-    .iterate_shared = nvmixReaddir,
+    // TODO 后续考虑共享式遍历的 iterate_shared 的修改实现。
+    .iterate = nvmixReaddir,
 };
 
 int nvmixReaddir(struct file *pDir, struct dir_context *pCtx)
@@ -32,7 +33,9 @@ int nvmixReaddir(struct file *pDir, struct dir_context *pCtx)
     struct super_block *pSb = NULL;
     struct NvmixInodeInfo *pNii = NULL;
     struct buffer_head *pBh = NULL;
+    struct NvmixDirEntry *pNde = NULL;
     int res = 0;
+    int isOver = 0;
 
 
     pInode = pDir->f_inode;
@@ -52,7 +55,27 @@ int nvmixReaddir(struct file *pDir, struct dir_context *pCtx)
     }
 
     // dir_context 是内核用于目录遍历操作的关键数据结构。它封装了遍历目录时的上下文信息。主要作用是在多次调用目录遍历函数（如 .iterate 或 .iterate_shared）时，保存遍历的进度和状态，确保每次调用能正确继续上一次的位置。
-    // TODO
+    for (; pCtx->pos <= NVMIX_MAX_ENTRY_NUM; ++pCtx->pos)
+    {
+        // 每个目录项的元数据是我自己定义的 NvmixDirEntry 类型。
+        pNde = (struct NvmixDirEntry *)pBh->b_data + pCtx->pos;
+
+        // inode 为 0 无效条目，需跳过。
+        if (0 == pNde->m_ino) continue;
+
+        // dir_emit() 用于在实现文件系统的 readdir 操作时，将有效的目录项信息填充到用户空间的缓冲区。返回非 0 值标识用户空间缓冲区已满或发生错误，此时需记录当前位置并跳出循环，以方便下次读取剩余条目。
+        isOver = dir_emit(pCtx, pNde->m_name, NVMIX_MAX_NAME_LENGTH, pNde->m_ino, DT_UNKNOWN);
+        if (isOver)
+        {
+            pr_info("nvmixfs: read %s from directory %s, ctx->pos: %lld\n", pNde->m_name, pDir->f_path.dentry->d_name.name, pCtx->pos);
+
+            ++pCtx->pos; // 当前 pos 位置读取完毕，跳到下个位置并跳出循环。
+
+            break;
+        }
+    }
+
+    brelse(pBh);
 
 
 ERR:

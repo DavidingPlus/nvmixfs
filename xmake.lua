4d48a3bc233a094aa2dd3d7ded27dd36bfb12139
DavidingPlus <davidingplus@qq.com>
2025-03-11 11:19:10 +0000

    fix typo

--- xmake.lua
+++ xmake.lua
@@ -4,11 +4,10 @@ option ("linux-headers", {showmenu = true, description = "Set linux-headers path
 add_includedirs ("src/")
 
 target ("nvmixfs")
-    set_languages ("gnu99")
     add_rules ("platform.linux.module")
     add_files ("src/*.c")
     set_values ("linux.driver.linux-headers", "$(linux-headers)")
-
+    set_languages ("gnu99")
     add_cflags ("-O1")
 
 

b7fe6994e5859a1ec391c2763d96d7cdc0c0b8d1
DavidingPlus <davidingplus@qq.com>
2025-03-21 16:25:37 +0000

    I made a mistake, wrong use for macro, now adding comments for it

--- src/cross-space/globalmacros.h
+++ src/cross-space/globalmacros.h
@@ -11,6 +11,7 @@
 #define _NVMIX_GLOBALMACROS_H_
 
 
+// 所有 cross-space 中的文件都会引用 globalmacros.h，因为这是个 C 库，不能保证用户态是用 C 还是 C++ 来测。C++ 的代码默认会定义 __cplusplus 宏，因此根据这个对 NVMIX_EXTERN_C_BEGIN 和 NVMIX_EXTERN_C_END 分别定义，防止符号兼容性出问题。可能想到的一种写法是将 #ifdef __cplusplus 写在 #define 中，但这样是非法的，必须使用条件编译的形式。
 #ifdef __cplusplus
 
 #define NVMIX_EXTERN_C_BEGIN \

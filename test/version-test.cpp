ae667f47d8a57fd50d4f25c8023d53318e82b4ec
DavidingPlus <davidingplus@qq.com>
2025-03-23 15:09:37 +0000

    add unit test for version

---
+++ test/version-test.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include <string>
+
+#include "config.h"
+
+
+TEST(VersionTest, Test1)
+{
+    std::cout << NVMIX_CONFIG_VERSION << std::endl;
+
+    std::cout << NVMIX_CONFIG_VERSION_MAJOR << std::endl;
+
+    std::cout << NVMIX_CONFIG_VERSION_MINOR << std::endl;
+
+    std::cout << NVMIX_CONFIG_VERSION_ALTER << std::endl;
+}
+
+TEST(VersionTest, Test2)
+{
+    char major = '0' + NVMIX_CONFIG_VERSION_MAJOR;
+    char minor = '0' + NVMIX_CONFIG_VERSION_MINOR;
+    char alter = '0' + NVMIX_CONFIG_VERSION_ALTER;
+
+    std::string version;
+
+    version += major;
+    version += '.';
+    version += minor;
+    version += '.';
+    version += alter;
+
+
+    EXPECT_EQ(NVMIX_CONFIG_VERSION, version);
+}

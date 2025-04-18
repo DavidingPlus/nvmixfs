ab6b3c7167a982e219054dfac38553e4e1ab5486
DavidingPlus <davidingplus@qq.com>
2025-04-18 15:59:20 +0000

    fix init.sh

--- snippet/MkfsNvmixfs/init.sh
+++ snippet/MkfsNvmixfs/init.sh
@@ -6,6 +6,12 @@ CURRENT_DIR="$(dirname "$(realpath "$0")")"
 
 $CURRENT_DIR/clear.sh
 
+cd $CURRENT_DIR/../../
+
+xmake f -m debug --linux-headers=/usr/src/linux-headers-5.4.0-214-generic/ --with-gtest=true
+
+xmake build
+
 sudo mkdir /mnt/nvmixfs/
 
 # 我本地预留了一段内存，起始物理地址是 0x100000000，大小是 0x10000000，即 256 M。
